4fe9b722013fcfe264485b6ac639ce51b208b1b9
peterX <74089102+Basi-mon@users.noreply.github.com>
2021-04-08 10:54:19 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -9,6 +9,8 @@ from helpers.filters import other_filters2
 async def start(_, message: Message):
     await message.reply_text(
         f"""[★](https://telegra.ph/file/740f87cbf8ef39d7af03d.png)I am **{bn}** A telegram voice chat bot for playing songs in your group voice chat
+Click [commands](https://telegra.ph/MUSIC-BOT-COMMAND-04-08) to look at bot commands
+add assistant user bot after adding bot to groups you can get it by clicking [GROUP] button
         """,
         reply_markup=InlineKeyboardMarkup(
             [
@@ -17,7 +19,7 @@ async def start(_, message: Message):
                         "SOURCE CODE", url="https://github.com/zyrus-basi/vc_heroku"
                     ),
                     InlineKeyboardButton(
-                        "GROUP||COMMANDS", url="https://t.me/unitedbotsupport"
+                        "GROUP||ASSISTANT", url="https://t.me/unitedbotsupport"
                     )
                 ]
             ]
