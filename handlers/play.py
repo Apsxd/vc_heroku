dcedf97d3e7e2f5eb6d7067c04b911874eac319a
peterX <74089102+Basi-mon@users.noreply.github.com>
2021-04-16 20:19:42 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -11,6 +11,7 @@ from downloaders import youtube
 from config import BOT_NAME as bn, DURATION_LIMIT
 from helpers.filters import command, other_filters
 from helpers.decorators import errors
+from helpers.errors import DurationLimitError
 from helpers.gets import get_url, get_file_name
 
 
