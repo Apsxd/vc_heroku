1cb6712a60f66ce34d942181bc5c37013f275c19
peterX <74089102+Basi-mon@users.noreply.github.com>
2021-05-03 18:30:35 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,9 +8,9 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f""",I am **{bn}** A telegram voice chat bot for playing songs in your group voice chat
-Click [commands](https://telegra.ph/MUSIC-BOT-COMMAND-04-08) to look at bot commands
-add assistant user bot after adding bot to groups you can get it by clicking [GROUP] button
+f""",I am **{bn}** A telegram voice chat bot for playing songs in your group voice chat.
+Click [COMMANDS](https://telegra.ph/MUSIC-BOT-COMMAND-04-08) For a takeup on commands.
+Join our support group for getting manual about how adding to your group
         """,
         reply_markup=InlineKeyboardMarkup(
             [
